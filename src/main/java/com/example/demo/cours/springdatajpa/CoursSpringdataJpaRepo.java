8137cd444bcc97cd1d74f42d7fd0bb04dfbcec1c
iheb meftah <ihebmeftah666@gmail.com>
2023-07-06 11:58:07 +0100

    can add methode in repo spring data jpa

--- src/main/java/com/example/demo/cours/springdatajpa/CoursSpringdataJpaRepo.java
+++ src/main/java/com/example/demo/cours/springdatajpa/CoursSpringdataJpaRepo.java
@@ -1,9 +1,13 @@
 package com.example.demo.cours.springdatajpa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.cours.Cours;
 
 public interface CoursSpringdataJpaRepo extends JpaRepository<Cours,Long> {
-    
+    List<Cours> findByAuthor(String author);
+    List<Cours> findByName(String name);
+
 }
