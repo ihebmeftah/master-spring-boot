1aefe0d4dc9d1232fd2584d193f5b6ef55bbf34d
iheb meftah <ihebmeftah666@gmail.com>
2023-07-07 06:50:04 +0100

    hello request mapping

---
+++ src/main/java/com/example/demo/hello/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @RequestMapping("sayhello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello spring boot application";
+    }
+
+}
