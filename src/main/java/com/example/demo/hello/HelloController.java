4f7344e71192d08d8ff3605dc83e7310ae95d5e4
iheb meftah <ihebmeftah666@gmail.com>
2023-07-10 18:12:21 +0100

    #

--- src/main/java/com/example/demo/hello/HelloController.java
+++ src/main/java/com/example/demo/hello/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.demo.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,10 @@ public class HelloController {
     @GetMapping("/")
     public Hello helloEndpoint(){
         return new Hello("hello world")  ;
+    }
+
+    @GetMapping("/{id}")
+    public Hello helloPathVariabaleEndpoint(@PathVariable String id){
+        return new Hello("hello world " + id)  ;
     }
 }
